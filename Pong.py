093c77bdd8704004cce50353ea03d449cf287689
mathias <mathias.ressort@epitech.eu>
2020-11-04 21:23:07 +0100

    [ADD] The arena

--- Pong.py
+++ Pong.py
@@ -1,6 +1,6 @@
 import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Canvas
 
 gui = tk.Tk()
 gui.geometry("800x500")
@@ -13,4 +13,32 @@ if "nt" == os.name:
 else:
     gui.wm_iconbitmap(bitmap = "@image/logo.xbm")
 
-gui.mainloop()
+PosX = 60
+PosY = 10
+PosX2 = 200
+PosY2 = 480
+dx = 10
+dy = 7
+
+def update():
+
+    def KeyBoard(event):
+        Key = event.keysym
+
+        if Key == 'Right':
+            canvas.move(raquette,30,0)
+        if Key == 'Left':
+            canvas.move(raquette,-30,0)
+
+    canvas = Canvas(gui,width = 500, height = 500 , bd=0, bg="grey")
+    canvas.pack(padx=10,pady=10)
+
+    raquette = canvas.create_rectangle(PosX2,PosY2,PosX2+100,PosY2+10,fill='black')
+
+
+    canvas.focus_set()
+    canvas.bind('<Key>',KeyBoard)
+
+    gui.mainloop()
+
+update()
